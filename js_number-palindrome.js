95f8bc27e4cce795f0865e539d839aa104f83757
ashirogisen <m.rizki.aiman@gmail.com>
2019-08-12 19:04:52 +0700

    number-palindrome fixed

--- js_number-palindrome.js
+++ js_number-palindrome.js
@@ -1,24 +1,24 @@
 function palindrome(kata) {
     // you can only write your code here!
-    var x = false;
-    var a = '';
-
-    for(var i = kata.length-1; i>=0; i--){
-        a = a + kata[i];
-    }
+    var x = '';
+    var str_kata = kata.toString();
+    console.log(kata);
     
-    if (kata === a) {
-        x = true;
-    } else {
-        x = false;
+    for(var i = str_kata.length-1; i >= 0; i--) {
+        x = x + str_kata[i];
     }
 
-    return x;
-  }
+    if (str_kata === x) {
+        return true
+    } else {
+        return false
+    }
+}
+   
 
 function angkaPalindrome(num) {
+    num = num+1;
     while(num > 0) {
-        num = num + 1;
         if (palindrome(num.toString()) === true) {
             return num
         }
@@ -28,11 +28,10 @@ function angkaPalindrome(num) {
     }
 }
 
-
-
 // TEST CASES
+console.log(palindrome(121));
 console.log(angkaPalindrome(8)); // 9
-// console.log(angkaPalindrome(10)); // 11
-// console.log(angkaPalindrome(117)); // 121
-// console.log(angkaPalindrome(175)); // 181
-// console.log(angkaPalindrome(1000)); // 1001
+console.log(angkaPalindrome(10)); // 11
+console.log(angkaPalindrome(117)); // 121
+console.log(angkaPalindrome(175)); // 181
+console.log(angkaPalindrome(1000)); // 1001

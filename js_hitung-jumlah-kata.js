0d13e0031f0d592f01b11959bc0d60af5ae689ed
ashirogisen <m.rizki.aiman@gmail.com>
2019-08-26 11:57:56 +0700

    updated hitung jumlah kata

--- js_hitung-jumlah-kata.js
+++ js_hitung-jumlah-kata.js
@@ -1,18 +1,33 @@
 function hitungJumlahKata(kalimat) {
     // you can only write your code here!
-    var x = kalimat.length;
-    var banyak_kata = 1;
-    for(var i = 0; i < x; i++){
+    var arr_kalimat = []
+    var x = ''
+    
+    for (var i = 0; i < kalimat.length; i++) {
         if (kalimat[i] === ' ') {
-            banyak_kata++
+            arr_kalimat.push(x)
+            x = ''
+        } else {
+            x = x + kalimat[i]
         }
     }
-    return banyak_kata;
+    arr_kalimat.push(x)
+
+    var arr_kalimat_freeSpace = []
+    for (var i = 0; i< arr_kalimat.length; i++) {
+        if (arr_kalimat[i] !== '') {
+            arr_kalimat_freeSpace.push(arr_kalimat[i])
+        }
+    }
+
+    // console.log(arr_kalimat_freeSpace);
+    return arr_kalimat_freeSpace.length
+    
   }
   
   // TEST CASES
   console.log(hitungJumlahKata('I have a dream')); // 4
-  console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
-  console.log(hitungJumlahKata('A song to sing')); // 4
-  console.log(hitungJumlahKata('I')); // 1
-  console.log(hitungJumlahKata('I believe I can code')); // 5
+//   console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
+//   console.log(hitungJumlahKata('A song to sing')); // 4
+//   console.log(hitungJumlahKata('I')); // 1
+//   console.log(hitungJumlahKata('I believe I can code')); // 5
